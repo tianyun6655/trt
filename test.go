14bf3cbeeae108a124fd57430cb53f705f06e38f
Tianyun <451617222@qq.com>
2018-11-18 03:59:22 -0800

    init

---
+++ test.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main(){
+	fmt.Println("Sucks")
+	fmt.Println("Hello world! By Sheldon")
+	fmt.Println("Hello world!")
+}
+
+func Subsets(nums []int) [][]int {
+	result:=make([][]int,0)
+	return result
+}
+
+
+
+
+
+func arvin(){
+	fmt.Println("arvin")
+}
